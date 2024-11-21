488f3ba95b99f0ede8b203da4d80d4e0e806ff8e
marcia_gisseli <marciagisseli21@gmail.com>
2024-11-21 08:29:17 -0300

    finalizando com o hamburguer

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -2,6 +2,7 @@ function abrirNav(){
     const menu = document.getElementById("offcanvas");
     // selecione um elemento atraves do ID
     menu.style.width = "250px"
+    console.log(menu)
 }
 
 function fecharNav(){
