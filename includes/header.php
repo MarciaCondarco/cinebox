c1b69cfb2fd717c429af7d859edf4bd8deaa8bfc
Marcy_ <marciagisseli21@gmail.com>
2025-03-28 08:59:09 -0300

    adicionando a estilização do detalhes

--- includes/header.php
+++ includes/header.php
@@ -30,6 +30,7 @@
     <link rel="stylesheet" href="./assets/css/sobre.css">
     <link rel="stylesheet" href="./assets/css/login.css">
     <link rel="stylesheet" href="./assets/css/filtro.css">
+    <link rel="stylesheet" href="./assets/css/detalhes.css">
 </head>
 
 <body>
